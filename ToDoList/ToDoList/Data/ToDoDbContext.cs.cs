d96e40bec9cd9fa2d67107d31605441d017efeca
theoners <theonebgrs@gmail.com>
2018-12-02 19:10:32 +0100

    added projects

---
+++ ToDoList/ToDoList/Data/ToDoDbContext.cs.cs
@@ -0,0 +1,18 @@
+﻿namespace ToDoList.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using ToDoList.Models;
+
+    public class ToDoDbContext : DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+
+        public const string ConnectionString = @"Server=localhost\SQLEXPRESS;Database=ToDoListDB;Trusted_Connection=True;";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+    }
+}
---
+++ ToDoList/ToDoList/Models/Task.cs
@@ -0,0 +1,16 @@
+﻿namespace ToDoList.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Task
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Comments { get; set; }
+    }
+}
